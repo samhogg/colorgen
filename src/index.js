78c0eb8f993e69d6f9fd31358fdee677c5e871eb
Sam Hogg <sam.hogg@orionhealth.com>
2016-03-04 13:31:47 +1300

    Implement an Iterator without using generators

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const GOLDEN_ANGLE = (4 - 2 * GOLDEN_RATIO) * Math.PI
 
 import d3Scale from 'd3-scale'
 
-export default function * ColorGenerator () {
+export default function ColorGenerator () {
   let i = 0
   const scale = d3Scale.scaleRainbow()
-  while (true) yield scale(Math.abs(Math.sin(GOLDEN_ANGLE * ++i)))
+  return {
+    next: () => ({
+      value: scale(Math.abs(Math.sin(GOLDEN_ANGLE * ++i))),
+      done: false
+    })
+  }
 }
